837afa2884b2724e2b7bb065429250e88fd69279
Mariam Reintop <MReintop@users.noreply.github.com>
2017-05-04 08:31:33 +0300

    seminar

---
+++ src/main/java/mariam/Euroks.java
@@ -0,0 +1,12 @@
+package mariam;
+
+public class Euroks{
+	
+	public double dollarEuroks(double x){
+		return x*0.917810105;
+	}
+	
+	public double rublaEuroks(double x){
+		return x*0.0161167454;
+	}
+}
